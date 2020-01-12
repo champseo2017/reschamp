5e8eccea7db18767eaaa4c69fd64b9defebb03b1
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2020-01-13 00:54:01 +0700

    update

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -1,107 +1,152 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import { locale } from "../locales/index";
 
 class Resume extends Component {
+  _isMounted = false;
   constructor(props) {
     super(props);
     // Don't call this.setState() here!
     this.state = {
-      Educationth:"การศึกษา",
-      schoolth:"มหาวิทยาลัย?",
-      degreeth:"มหาวิทยาลัยแม่โจ้ ปริญญาตรี",
-      graduatedth:"กุมภาพันธ์ 2016"
+      Educationth: "การศึกษา",
+      schoolth: "มหาวิทยาลัย?",
+      degreeth: "มหาวิทยาลัยแม่โจ้ ปริญญาตรี",
+      graduatedth: "กุมภาพันธ์ 2016",
+      UniversityDetailsen: null
     };
     //this.handleClick = this.handleClick.bind(this);
   }
+  componentDidMount() {
+    this._isMounted = true;
+  }
   componentWillReceiveProps(nextProps) {
-    if (nextProps.datalanth === "th") {
-      this.setState({
-        Educationth: locale.t("Educationth"),
-        schoolth: locale.t("schoolth"),
-        degreeth: locale.t("degreeth"),
-        graduatedth: locale.t("graduatedth"),
-      });
-    } else if (nextProps.datalanen === "en") {
-      this.setState({
-        Educationth: locale.t("Educationen"),
-        schoolth: locale.t("schoolen"),
-        degreeth: locale.t("degreeen"),
-        graduatedth: locale.t("graduateden"),
-      });
+    if (
+      nextProps.datalanth !== this.props.datalanth &&
+      nextProps.datalanth === "th"
+    ) {
+      if (this._isMounted) {
+        let copystate = { ...this.state };
+        copystate.UniversityDetailsen = null;
+        this.setState({
+          Educationth: locale.t("Educationth"),
+          schoolth: locale.t("schoolth"),
+          degreeth: locale.t("degreeth"),
+          graduatedth: locale.t("graduatedth"),
+          UniversityDetailsen: copystate.UniversityDetailsen
+        });
+      }
+    } else if (
+      nextProps.datalanen !== this.props.datalanen &&
+      nextProps.datalanen === "en"
+    ) {
+      if (this._isMounted) {
+        let copystate = { ...this.state };
+        copystate.UniversityDetailsen = locale.t("UniversityDetailsen");
+        this.setState({
+          Educationth: locale.t("Educationen"),
+          schoolth: locale.t("schoolen"),
+          degreeth: locale.t("degreeen"),
+          graduatedth: locale.t("graduateden"),
+          UniversityDetailsen: copystate.UniversityDetailsen
+        });
+      }
     }
   }
+  componentWillUnmount() {
+    this._isMounted = false;
+  }
   render() {
-    var {Educationth,schoolth,degreeth,graduatedth} = this.state
-    if(this.props.data){
+    var {
+      Educationth,
+      schoolth,
+      degreeth,
+      graduatedth,
+      UniversityDetailsen
+    } = this.state;
+
+    if (this.props.data) {
       var skillmessage = this.props.data.skillmessage;
-      var education = this.props.data.education.map(function(education){
-        return <div key={education.school}><h3>{schoolth}</h3>
-        <p className="info">{degreeth} <span>&bull;</span><em className="date">{graduatedth}</em></p>
-        <p>{education.description}</p></div>
-      })
-      var work = this.props.data.work.map(function(work){
-        return <div key={work.company}><h3>{work.company}</h3>
-            <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
+      var education = this.props.data.education.map(function(education) {
+        return (
+          <div key={education.school}>
+            <h3>{schoolth}</h3>
+            <p className="info">
+              {degreeth} <span>&bull;</span>
+              <em className="date">{graduatedth}</em>
+            </p>
+            <p>
+              {!UniversityDetailsen
+                ? education.description
+                : UniversityDetailsen}
+            </p>
+          </div>
+        );
+      });
+      var work = this.props.data.work.map(function(work) {
+        return (
+          <div key={work.company}>
+            <h3>{work.company}</h3>
+            <p className="info">
+              {work.title}
+              <span>&bull;</span> <em className="date">{work.years}</em>
+            </p>
             <p>{work.description}</p>
-        </div>
-      })
-      var skills = this.props.data.skills.map(function(skills){
-        var className = 'bar-expand '+skills.name.toLowerCase();
-        return <li key={skills.name}><span style={{width:skills.level}}className={className}></span><em>{skills.name}</em></li>
-      })
+          </div>
+        );
+      });
+      var skills = this.props.data.skills.map(function(skills) {
+        var className = "bar-expand " + skills.name.toLowerCase();
+        return (
+          <li key={skills.name}>
+            <span style={{ width: skills.level }} className={className}></span>
+            <em>{skills.name}</em>
+          </li>
+        );
+      });
     }
 
     return (
       <section id="resume">
+        <div className="row education">
+          <div className="three columns header-col">
+            <h1>
+              <span>{Educationth}</span>
+            </h1>
+          </div>
 
-      <div className="row education">
-         <div className="three columns header-col">
-    <h1><span>{Educationth}</span></h1>
-         </div>
-
-         <div className="nine columns main-col">
+          <div className="nine columns main-col">
             <div className="row item">
-               <div className="twelve columns">
-                 {education}
-               </div>
+              <div className="twelve columns">{education}</div>
             </div>
-         </div>
-      </div>
-
-
-      <div className="row work">
-
-         <div className="three columns header-col">
-            <h1><span>Work</span></h1>
-         </div>
-
-         <div className="nine columns main-col">
-          {work}
+          </div>
         </div>
-    </div>
 
+        <div className="row work">
+          <div className="three columns header-col">
+            <h1>
+              <span>Work</span>
+            </h1>
+          </div>
 
+          <div className="nine columns main-col">{work}</div>
+        </div>
 
-      <div className="row skill">
+        <div className="row skill">
+          <div className="three columns header-col">
+            <h1>
+              <span>Skills</span>
+            </h1>
+          </div>
 
-         <div className="three columns header-col">
-            <h1><span>Skills</span></h1>
-         </div>
+          <div className="nine columns main-col">
+            <p>{skillmessage}</p>
 
-         <div className="nine columns main-col">
-
-            <p>{skillmessage}
-            </p>
-
-				<div className="bars">
-				   <ul className="skills">
-					  {skills}
-					</ul>
-				</div>
-			</div>
-      </div>
-   </section>
+            <div className="bars">
+              <ul className="skills">{skills}</ul>
+            </div>
+          </div>
+        </div>
+      </section>
     );
   }
 }
@@ -112,6 +157,5 @@ function mapStateToProps(state) {
     datalanen: state.lanenReducers.lanen.data
   };
 }
-
 
 export default connect(mapStateToProps)(Resume);
